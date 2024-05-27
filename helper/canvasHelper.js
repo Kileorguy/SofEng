fe244f1e8d7463dd72cd3cb404aea5aecfe38482
Kevin Alexander Andreas <Sznxnzu@gmail.com>
2024-05-27 22:25:07 +0700

    Add Bacground Function
    Needs tweaking, no background yet

--- helper/canvasHelper.js
+++ helper/canvasHelper.js
@@ -2,7 +2,26 @@ import {Game} from "../scripts/gameLogic.js";
 
 export function clearCanvas(ctx) {
     // let game = Game.getInstance()
-    ctx.fillStyle = "green"
-    ctx.fillRect(0,0,Game.canvasWidth,Game.canvasHeight)
+    // ctx.fillStyle = "green"
+    // ctx.fillRect(0,0,Game.canvasWidth,Game.canvasHeight)
+
+
+
+    // ctx = canvas.getContext("2d");
+
+    // Background Function
+    const image = new Image();
+    image.onload = drawImageActualSize; 
+
+    image.src = "/assets/backgrounds/test.jpg";
+    function drawImageActualSize() 
+    {
+        canvas.width = Game.canvasWidth;
+        canvas.height = Game.canvasHeight;
+
+        ctx.drawImage(this, 0, 0);
+        console.log(Game.canvasHeight);
+        console.log(Game.canvasWidth);
+    }
 }
 
